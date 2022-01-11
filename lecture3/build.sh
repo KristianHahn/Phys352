e7ea15e6e08821454658ad02aec3acfed86c9993
Kristian Hahn <khahn@cern.ch>
2022-01-11 08:17:48 -0600

    explicit build script

---
+++ lecture3/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+gcc address.c -o address.exe
+gcc convert2.c -o convert2.exe
+gcc convertWithHeader.c -o convertWithHeader.exe
+gcc doItAgain.c -o doItAgain.exe
+gcc doIt.c -o doIt.exe
+gcc doNothing.c -o doNothing.exe
+gcc global.c -o global.exe
+gcc globalFunctions.c -o globalFunctions.exe
+gcc helloProto.c -o helloProto.exe
+gcc helloWithHeader.c -o helloWithHeader.exe
+gcc init.c -o init.exe
+gcc localScope.c -o localScope.exe
+gcc pointerArithmetic.c -o pointerArithmetic.exe
+gcc pointer.c -o pointer.exe
+gcc pointerForArrays.c -o pointerForArrays.exe
+gcc pointerFunctions.c -o pointerFunctions.exe
+gcc quicksort.c -o quicksort.exe
+gcc whileScope.c -o whileScope.exe
