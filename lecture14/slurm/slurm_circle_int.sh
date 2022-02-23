3a2eeaa8ace52f7ed4fd23543a61bf5c30b077f5
Kristian Hahn <khahn@cern.ch>
2022-02-23 17:11:57 -0600

    fix script

--- lecture14/slurm/slurm_circle_int.sh
+++ lecture14/slurm/slurm_circle_int.sh
@@ -5,17 +5,15 @@
 #SBATCH -N 1                    # Number of Nodes
 #SBATCH --mem=18G               # Memory per node in GB needed for a job. Also see --mem-per-cpu
 #SBATCH --ntasks-per-node=6     # Number of Cores (Processors)
-#SBATCH --mail-user=<my_email>  # Designate email address for job communications
-#SBATCH --mail-type=<event>     # Events options are job BEGIN, END, NONE, FAIL, REQUEUE
-#SBATCH --output=<file_path>    # Path for output must already exist
-#SBATCH --error=<file_path>     # Path for errors must already exist
-#SBATCH --job-name="test"       # Name of job
+#SBATCH --output=./slurm/logs/%A_%a.out   # Path for output must already exist
+#SBATCH --error=./slurm/logs/%A_%a.err    # Path for errors must already exist
+#SBATCH --job-name="circleInt"  # Name of job
 #SBATCH --array 0-99            # Job array 
 
 
 
 # the job ...
-NTrails=1000000000
+NTrials=1000000000
 id=${SLURM_ARRAY_TASK_ID}
 seed=`echo "${id}*${RANDOM}" | bc`
 ./circleInt.exe ${Ntrials} ${seed} >& ./slurm/output/circle_${id}.dat
